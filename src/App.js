557b3145dbbfd96c0238c21f888dd1a527d132cf
asp2131 <akinpds@gmail.com>
2019-08-04 13:00:55 -0500

    Render Material appbar onto Homepage Ui

--- src/App.js
+++ src/App.js
@@ -1,11 +1,10 @@
 import React from 'react';
 import './App.css';
-
+import MenuAppBar from './components/Nav.js'
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-      </header>
+      <MenuAppBar />
     </div>
   );
 }

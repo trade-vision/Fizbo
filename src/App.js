b4d998e44a1dd8e015ffb48c84a7151a0be97a57
asp2131 <akinpds@gmail.com>
2019-08-08 18:52:48 -0500

    Appbar appears on all views

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import './App.css';
-import MenuAppBar from './components/Nav.js'
+// import MenuAppBar from './components/Nav.js'
 import Welcome from './components/Welcome.js'
 import PropertyList from './components/PropertyList.js'
 import SignInView from './components/SignIn.js'
@@ -12,7 +12,6 @@ import { BrowserRouter, Route, Switch } from 'react-router-dom';
 function App() {
   return (
     <div className="App">
-      <MenuAppBar />
       <Welcome />
       <PropertyList />    
     </div>

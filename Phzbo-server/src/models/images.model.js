dee36eb8ebfc7838af55364eb50641f71a9187e6
Eric ONeal Jr <ejeric23@gmail.com>
2019-08-12 15:42:42 -0500

    fixingApi

---
+++ Phzbo-server/src/models/images.model.js
@@ -0,0 +1,25 @@
+// See http://docs.sequelizejs.com/en/latest/docs/models-definition/
+// for more of what you can do here.
+const Sequelize = require('sequelize');
+const DataTypes = Sequelize.DataTypes;
+
+module.exports = function (app) {
+  const sequelizeClient = app.get('sequelizeClient');
+  const images = sequelizeClient.define('images', {
+    url: {type: DataTypes.STRING, allowNull: true} 
+  }, {
+    hooks: {
+      beforeCount(options) {
+        options.raw = true;
+      }
+    }
+  });
+
+  // eslint-disable-next-line no-unused-vars
+  images.associate = function (models) {
+    // Define associations here
+    // See http://docs.sequelizejs.com/en/latest/docs/associations/
+  };
+
+  return images;
+};
--- Phzbo-server/src/services/index.js
+++ Phzbo-server/src/services/index.js
@@ -1,5 +1,9 @@
 const user = require('./user/user.service.js');
+const listings = require('./listings/listings.service.js');
+const images = require('./images/images.service.js');
 // eslint-disable-next-line no-unused-vars
 module.exports = function (app) {
   app.configure(user);
+  app.configure(listings);
+  app.configure(images);
 };

bd9a2c478a893d85d656e1e312609c034643c1ad
asp2131 <akinpds@gmail.com>
2019-08-29 12:58:52 -0500

    Got rid of unsused components

--- src/Router.js
+++ src/Router.js
@@ -1,7 +1,5 @@
 import React, {useState, useEffect} from 'react';
 import App from './App.js';
-import SignInView from './components/user/SignIn.js'
-import SignUpView from './components/user/SignUp.js'
 import UserProfile from './components/user/UserProfile.js'
 import Nav from './components/Nav'
 import axios from 'axios';
@@ -37,8 +35,6 @@ function Router() {
                 <Nav user={user} />
                 <Switch>  
                     <Route path="/" render={() => <App user={user}/>} exact />
-                    <Route exact path="/signIn" render={() => <SignInView />} />
-                    <Route exact path="/signUp" render={() => <SignUpView />} />
                     <Route exact path="/userProfile" render={() => <UserProfile />} />
                 </ Switch>
 
